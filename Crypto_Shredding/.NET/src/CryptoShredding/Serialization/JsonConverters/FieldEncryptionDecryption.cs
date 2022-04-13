dc6b2d0ea87179e8c1bcb5b17720488b35e66003
Oskar Dudycz <oskar.dudycz@gmail.com>
2022-04-13 09:22:14 +0200

    Added tiny refactoring of Crypto Shredding sample to make code a bit clearer

--- Crypto_Shredding/.NET/src/CryptoShredding/Serialization/JsonConverters/FieldEncryptionDecryption.cs
+++ Crypto_Shredding/.NET/src/CryptoShredding/Serialization/JsonConverters/FieldEncryptionDecryption.cs
@@ -11,64 +11,64 @@ public class FieldEncryptionDecryption
         
     public object GetEncryptedOrDefault(object value, ICryptoTransform encryptor)
     {
-        if (encryptor is null)
-        {
+        if (encryptor is null) 
             throw new ArgumentNullException(nameof(encryptor));
-        }
+        
         var isEncryptionNeeded = value != null;
-        if (isEncryptionNeeded)
-        {
-            using var memoryStream = new MemoryStream();
-            using var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
-            using var writer = new StreamWriter(cryptoStream);
-            var valueAsText = value.ToString();
-            writer.Write(valueAsText);
-            writer.Flush();
-            cryptoStream.FlushFinalBlock();
+        
+        if (!isEncryptionNeeded) return default;
+        
+        using var memoryStream = new MemoryStream();
+        using var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+        using var writer = new StreamWriter(cryptoStream);
+        var valueAsText = value.ToString();
+        writer.Write(valueAsText);
+        writer.Flush();
+        cryptoStream.FlushFinalBlock();
 
-            var encryptedData = memoryStream.ToArray();
-            var encryptedText = Convert.ToBase64String(encryptedData);
-            var result = $"{EncryptionPrefix}{encryptedText}";
-            return result;
-        }
+        var encryptedData = memoryStream.ToArray();
+        var encryptedText = Convert.ToBase64String(encryptedData);
+        var result = $"{EncryptionPrefix}{encryptedText}";
+        
+        return result;
 
-        return default;
     }
 
     public object GetDecryptedOrDefault(object value, ICryptoTransform decryptor, Type destinationType)
     {
         if (value is null)
-        {
             throw new ArgumentNullException(nameof(value));
-        }
+        
         var isText = value is string;
-        if (isText)
+        if (!isText) return value;
+        
+        var valueAsText = (string)value;
+        var isEncrypted = valueAsText.StartsWith(EncryptionPrefix);
+
+        if (!isEncrypted)
         {
-            var valueAsText = (string)value;
-            var isEncrypted = valueAsText.StartsWith(EncryptionPrefix);
-            if (isEncrypted)
-            {
-                var isDecryptorAvailable = decryptor != null;
-                if (isDecryptorAvailable)
-                {
-                    var startIndex = EncryptionPrefix.Length;
-                    var valueWithoutPrefix = valueAsText.Substring(startIndex);
-                    var encryptedValue = Convert.FromBase64String(valueWithoutPrefix);
-                    using var memoryStream = new MemoryStream(encryptedValue);
-                    using var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
-                    using var reader = new StreamReader(cryptoStream);
-                    var decryptedText = reader.ReadToEnd();
-                    var result = Parse(destinationType, decryptedText);
-                    return result;
-                }
-                var maskedValue = GetMaskedValue(destinationType);
-                return maskedValue;
-            }
             var valueParsed = Parse(destinationType, valueAsText);
             return valueParsed;
         }
 
-        return value;
+        var isDecryptorAvailable = decryptor != null;
+        if (!isDecryptorAvailable)
+        {
+            var maskedValue = GetMaskedValue(destinationType);
+            return maskedValue;
+        }
+
+        var startIndex = EncryptionPrefix.Length;
+        var valueWithoutPrefix = valueAsText.Substring(startIndex);
+        var encryptedValue = Convert.FromBase64String(valueWithoutPrefix);
+        
+        using var memoryStream = new MemoryStream(encryptedValue);
+        using var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+        using var reader = new StreamReader(cryptoStream);
+        
+        var decryptedText = reader.ReadToEnd();
+        var result = Parse(destinationType, decryptedText);
+        return result;
     }
 
     private object Parse(Type outputType, string valueAsString)
