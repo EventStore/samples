bbebe5973df9e11f43a8a4cc4083c9374875318a
Richard Bennett <inbox@richardbennett.dev>
2022-05-02 15:18:23 -0400

    Adds stream reader/writer code.

---
+++ QuickStart/ReadWriteStream/StreamReader/Program.cs
@@ -0,0 +1,26 @@
+﻿// basic connection to EventStore DB
+using System.Text;
+
+using EventStore.Client;
+
+var settings = EventStoreClientSettings.Create("esdb://localhost:2113?tls=false");
+settings.DefaultCredentials = new UserCredentials("admin", "changeit");
+var client = new EventStoreClient(settings);
+Console.WriteLine("EventStore DB Connection setup.");
+
+var reader = client.ReadStreamAsync(
+    direction: Direction.Forwards,
+    streamName: "users",
+    StreamPosition.Start
+);
+
+await foreach(var e in reader) {
+    var metadata = Encoding.UTF8.GetString(e.Event.Metadata.ToArray());
+    var data = Encoding.UTF8.GetString(e.Event.Data.ToArray());
+
+    Console.WriteLine($"Event Position: {e.OriginalEventNumber}");
+    Console.WriteLine($"Event Metadata: {metadata}");
+    Console.WriteLine($"Event Data: {data}\n");
+}
+
+Console.WriteLine("Read completed.");
---
+++ QuickStart/ReadWriteStream/StreamWriter/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Json;
+
+using EventStore.Client;
+
+// data elements of two events to be stored within EventStore DB
+var events = new[]{
+    new {
+        Type = "NewUser",
+        Metadata = new{
+            Date = DateTime.Now
+        },
+        Body = new {
+            Id = Guid.NewGuid(),
+            FirstName = "George",
+            LastName = "Washington"
+        }
+    },
+    new {
+        Type = "NewUser",
+        Metadata = new{
+            Date = DateTime.Now
+        },
+        Body = new {
+            Id = Guid.NewGuid(),
+            FirstName = "Samuel",
+            LastName = "Adams"
+        }
+    }
+};
+
+// basic connection to EventStore DB
+var settings = EventStoreClientSettings.Create("esdb://localhost:2113?tls=false");
+settings.DefaultCredentials = new UserCredentials("admin", "changeit");
+var client = new EventStoreClient(settings);
+Console.WriteLine("EventStore DB Connection setup.");
+
+// Serialization of the above data.
+var serialized = events.Select(e => new EventData(Uuid.NewUuid(), e.Type, JsonSerializer.SerializeToUtf8Bytes(e.Body), JsonSerializer.SerializeToUtf8Bytes(e.Metadata))).ToArray();
+Console.WriteLine("Events are serialized and ready for storage.");
+
+// storing the serialized events within EventStore DB
+await client.AppendToStreamAsync(
+    streamName: "users", 
+    expectedRevision: StreamRevision.None, 
+    eventData: serialized);
+Console.WriteLine("Events have been stored.");
