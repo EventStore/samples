63869ce31083804f25f4a8496dab42fe0ee5f38a
josephcummings <josephjcummings@outlook.com>
2024-01-17 16:50:58 +0000

    Add Node.js sample hello world application

---
+++ Quickstart/Nodejs/esdb-sample-nodejs/app.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const {
+    EventStoreDBClient,
+    jsonEvent,
+    FORWARDS,
+    START,
+} = require("@eventstore/db-client");
+
+const eventStore = new EventStoreDBClient(
+    { endpoint: 'esdb_local:2113' }, 
+    { insecure: true }
+)
+
+const visitorsStream = 'visitors-stream'
+
+app.get('/hello-world', async (req, res) => {
+    const visitor = req.query.visitor ?? 'Visitor'
+
+    const event = jsonEvent({
+        type: 'VisitorGreeted',
+        data: {
+            visitor,
+        },
+    })
+
+    await eventStore.appendToStream(visitorsStream, [event])
+
+    const eventStream = eventStore.readStream(visitorsStream, {
+        fromRevision: START,
+        direction: FORWARDS,
+    })
+
+    let visitorsGreeted = []
+    for await (const { event } of eventStream)
+        visitorsGreeted.push(event.data.visitor)
+
+    res.send(`${visitorsGreeted.length} visitors have been greeted, they are: [${visitorsGreeted.join(',')}]`)
+})
+
+app.listen(port, () => {
+    console.log(`Sample app listening on port ${port}`)
+})
