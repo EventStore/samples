a854b5e5f15bcae505393d463433617ef1356511
Joseph Cummings <josephjcummings@outlook.com>
2024-02-08 11:19:35 +0000

    Register Gson mapper as a Bean/Singleton

--- Quickstart/Java/esdb-sample-springboot/src/main/java/com/example/esdbsamplespringboot/HelloWorldApplication.java
+++ Quickstart/Java/esdb-sample-springboot/src/main/java/com/example/esdbsamplespringboot/HelloWorldApplication.java
@@ -1,11 +1,18 @@
 package com.example.esdbsamplespringboot;
 
+import com.google.gson.Gson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HelloWorldApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+
+    @Bean
+    public Gson GsonMapper() {
+        return new Gson();
+    }
 }
